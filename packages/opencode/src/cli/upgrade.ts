9bacad9de9d0fe22e2e3aa8ee39ff769f9c0c5a7
hu_kangwei <hu_kangwei@hopedove.com>
2026-05-11 09:14:12 +0800

    忽略版本更新

--- packages/opencode/src/cli/upgrade.ts
+++ packages/opencode/src/cli/upgrade.ts
@@ -4,28 +4,29 @@ import { Flag } from "@/flag/flag"
 import { Installation } from "@/installation"
 
 export async function upgrade() {
-  const config = await Config.getGlobal()
-  const method = await Installation.method()
-  const latest = await Installation.latest(method).catch(() => {})
-  if (!latest) return
+  return false;
+  // const config = await Config.getGlobal()
+  // const method = await Installation.method()
+  // const latest = await Installation.latest(method).catch(() => {})
+  // if (!latest) return
 
-  if (Flag.OPENCODE_ALWAYS_NOTIFY_UPDATE) {
-    await Bus.publish(Installation.Event.UpdateAvailable, { version: latest })
-    return
-  }
+  // if (Flag.OPENCODE_ALWAYS_NOTIFY_UPDATE) {
+  //   await Bus.publish(Installation.Event.UpdateAvailable, { version: latest })
+  //   return
+  // }
 
-  if (Installation.VERSION === latest) return
-  if (config.autoupdate === false || Flag.OPENCODE_DISABLE_AUTOUPDATE) return
+  // if (Installation.VERSION === latest) return
+  // if (config.autoupdate === false || Flag.OPENCODE_DISABLE_AUTOUPDATE) return
 
-  const kind = Installation.getReleaseType(Installation.VERSION, latest)
+  // const kind = Installation.getReleaseType(Installation.VERSION, latest)
 
-  if (config.autoupdate === "notify" || kind !== "patch") {
-    await Bus.publish(Installation.Event.UpdateAvailable, { version: latest })
-    return
-  }
+  // if (config.autoupdate === "notify" || kind !== "patch") {
+  //   await Bus.publish(Installation.Event.UpdateAvailable, { version: latest })
+  //   return
+  // }
 
-  if (method === "unknown") return
-  await Installation.upgrade(method, latest)
-    .then(() => Bus.publish(Installation.Event.Updated, { version: latest }))
-    .catch(() => {})
+  // if (method === "unknown") return
+  // await Installation.upgrade(method, latest)
+  //   .then(() => Bus.publish(Installation.Event.Updated, { version: latest }))
+  //   .catch(() => {})
 }
